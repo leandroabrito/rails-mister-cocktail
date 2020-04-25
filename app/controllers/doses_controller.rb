0f761578f2afc89888b290fc2a62d0103b438969
leandro brito <leandroabrito89@gmail.com>
2020-04-25 16:29:26 -0300

    work on adding dose feature

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,25 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktails_path
+  end
+
+  def destroy
+    @dose = Dose.new(dose_params)
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# A user can see the list of cocktails
+# A user can see the details of a given cocktail, with the dose needed for each ingredient
+# A user can create a new cocktail
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
+
 end
