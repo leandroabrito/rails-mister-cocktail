68e60a5d211e487d18234225fd1c4639787c6c2d
leandro brito <leandroabrito89@gmail.com>
2020-04-28 20:48:12 -0300

    add root to route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 # A user can see the list of cocktails
 # A user can see the details of a given cocktail, with the dose needed for each ingredient
 # A user can create a new cocktail
+  root 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
